0c9075de072b2754349b7a82a5c12879988ab284
only2dhir <only2dhir@gmail.com>
2018-03-08 23:43:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  greetings: string[] = [];
+  showConversation: boolean = false;
+  ws: any;
+  name: string;
+  disabled: boolean;
+
+  constructor(){}
+
+  connect() {
+    //connect to stomp where stomp endpoint is exposed
+    //let ws = new SockJS(http://localhost:8080/greeting);
+    let socket = new WebSocket("ws://localhost:8080/greeting");
+    this.ws = Stomp.over(socket);
+    let that = this;
+    this.ws.connect({}, function(frame) {
+      that.ws.subscribe("/errors", function(message) {
+        alert("Error " + message.body);
+      });
+      that.ws.subscribe("/topic/reply", function(message) {
+        console.log(message)
+        that.showGreeting(message.body);
+      });
+      that.disabled = true;
+    }, function(error) {
+      alert("STOMP error " + error);
+    });
+  }
+
+  disconnect() {
+    if (this.ws != null) {
+      this.ws.ws.close();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
+  }
+
+  sendName() {
+    var data = JSON.stringify({
+      'name' : this.name
+    })
+    this.ws.send("/app/message", {}, data);
+  }
+
+  showGreeting(message) {
+    this.showConversation = true;
+    this.greetings.push(message)
+  }
+
+  setConnected(connected) {
+    this.disabled = connected;
+    this.showConversation = connected;
+    this.greetings = [];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
